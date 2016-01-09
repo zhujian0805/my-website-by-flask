31d38cd81e627e8d6feabe0c0c652df9c8abd9ed
Nemo Zhu <zhujian0805@gmail.com>
2016-01-09 20:49:15 +0800

    D  LICENSE D  README.md D  app/__init__.py D  app/__init__.pyc D  app/forms.py D  app/forms.pyc D  app/models.py D  app/models.pyc D  app/templates/base.html D  app/templates/index.html D  app/templates/login.html D  app/views.py D  app/views.pyc D  config.py D  config.pyc D  db_create.py D  db_downgrade.py D  db_migrate.py D  db_repository/README D  db_repository/__init__.py D  db_repository/manage.py D  db_repository/migrate.cfg D  db_repository/versions/001_migration.py D  db_repository/versions/002_migration.py D  db_repository/versions/__init__.py D  db_upgrade.py D  run.py D  setup.py D  tmp/.gitignore D  virtualenv.py

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-    username = TextField('username', validators = [Required()])
-    password = TextField('password', validators = [Required()])
-    remember_me = BooleanField('remember_me', default = False)
-    
