20ea125fc9551042ca0f841fa82aff85f8db7b30
Nemo Zhu <zhujian0805@gmail.com>
2016-01-10 21:33:10 +0800

    M app/models.py  M app/models.pyc  M app/views.py  M app/views.pyc  M create_data.py ?? app/forms.py ?? app/forms.pyc ?? app/templates/edit.html ?? app/templates/post.html ?? app/templates/user.html ?? db_repository/versions/003_migration.py ?? db_repository/versions/003_migration.pyc

--- flaskr/app/views.py
+++ flaskr/app/views.py
@@ -8,6 +8,10 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from app import models, db
 import os
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask import render_template, flash, redirect, session, url_for, request, g
+from forms import EditForm
+
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -18,6 +22,10 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/')
 @app.route('/index')
@@ -38,6 +46,22 @@ def index():
         user = user,
         posts = posts)
 
+@app.route('/user/<nickname>')
+def user(nickname):
+    user = models.User.query.filter_by(nickname = nickname).first()
+    print user
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        { 'author': user, 'body': 'Test post #1' },
+        { 'author': user, 'body': 'Test post #2' }
+    ]
+    print "Start to render", posts
+    return render_template('user.html',
+        user = user,
+        posts = posts)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -49,7 +73,8 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
+            g.user = current_user
+            return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
 
@@ -58,3 +83,7 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    return render_template('edit.html')
