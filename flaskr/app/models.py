439f6db0cd6f12b716836493d44793119fb3ef1c
James Zhu <zhujian0805@gmail.com>
2016-01-10 16:25:45 +0800

    M app/models.py  M app/models.pyc ?? db_repository/versions/007_migration.py ?? db_repository/versions/007_migration.pyc ?? db_repository/versions/008_migration.py ?? db_repository/versions/008_migration.pyc

--- flaskr/app/models.py
+++ flaskr/app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     name = db.Column(db.String(120))
+    test1= db.Column(db.String(120))
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
