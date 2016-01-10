c7ab9a036e54158996183d535a3020bf5ba6c29e
Nemo Zhu <zhujian0805@gmail.com>
2016-01-10 16:03:49 +0800

    M app/models.pyc ?? app01/

---
+++ flaskr/app01/models.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+class Entries(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(64), index = True, unique = True)
+    text = db.Column(db.String(20000))
+
+    def __repr__(self):
+        return '<Entries %r>' % (self.title)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
