0d9133aa50aedee428438d18775a4a026a1b2dfb
Nemo Zhu <zhujian0805@gmail.com>
2016-01-10 15:50:21 +0800

    M example_repository/manage.py  M example_repository/versions/001_Add_account_table.pyc  D example_repository/versions/002_Add_email_column.py  D example_repository/versions/002_Add_email_column.pyc  D example_repository/versions/003_alter_column.py  D example_repository/versions/003_alter_column.pyc

--- flaskr/example_repository/manage.py
+++ flaskr/example_repository/manage.py
@@ -2,4 +2,4 @@
 from migrate.versioning.shell import main
 
 if __name__ == '__main__':
-    main(url='mysql://root:123456@localhost:3306/example', debug='False', repository='example_repository')
+    main(debug='False')
