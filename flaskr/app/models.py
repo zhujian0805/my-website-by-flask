e4dea3c553c610360b0b28625d9acd0980708fd0
Nemo Zhu <zhujian0805@gmail.com>
2016-01-10 22:07:13 +0800

    M app/models.py  M app/models.pyc ?? db_repository/versions/004_migration.py ?? db_repository/versions/004_migration.pyc ?? db_repository/versions/005_migration.py ?? db_repository/versions/005_migration.pyc

--- flaskr/app/models.py
+++ flaskr/app/models.py
@@ -4,6 +4,7 @@ from hashlib import md5
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), unique = True)
+    fullname = db.Column(db.String(64))
     email = db.Column(db.String(120), index = True, unique = True)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
     about_me = db.Column(db.String(140))
