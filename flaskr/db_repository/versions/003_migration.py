883d26e0cec3b8dec7986f8ce325121915ac79f2
James Zhu <zhujian0805@gmail.com>
2016-01-11 10:48:47 +0800

    M app/models.py  M app/models.pyc ?? db_repository/versions/003_migration.py ?? db_repository/versions/003_migration.pyc

---
+++ flaskr/db_repository/versions/003_migration.py
@@ -0,0 +1,31 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+user = Table('user', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('nickname', String(length=64)),
+    Column('fullname', String(length=64)),
+    Column('email', String(length=120)),
+    Column('about_me', String(length=100)),
+    Column('test', String(length=100)),
+    Column('last_seen', DateTime),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['test'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['test'].drop()
