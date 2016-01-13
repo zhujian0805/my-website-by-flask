e2520c7dcf22e1b01884d01e8bb2fd03ca7291f2
James Zhu <zhujian0805@gmail.com>
2016-01-13 15:37:23 +0800

    M app/__init__.py  M app/__init__.pyc  M app/models.py  M app/models.pyc  M app/views.py  M app/views.pyc  M run.py

--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -1,8 +1,17 @@
+#import sys
+#sys.path.append("/home/jzhu/my-website-by-flask/flaskr")
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+from flask_admin import Admin
+# Flask and Flask-SQLAlchemy initialization here
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
+admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.Post, db.session))
